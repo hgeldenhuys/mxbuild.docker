e0c4392babacbbcb956d6d6a2053c84df3b5e888
hgeldenhuys <hgeldenhuys@gmail.com>
2019-09-25 01:30:49 -0400

    Added support for Mendix 6.10.10 and templatified Dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # mxbuild.docker
-Mendix Build Docker Recipes
+Mendix Build Docker Helper/Commandline Utility
 
+## Usage: ToDo
+
+## Downloading a distribution
+`ts-node src/index.ts --download-version 8.1.1`
+
+## Distribution location
+Most of the distribution files are uploaded to a temporary private server hosted at files.herman.codes,
+but these files can be hosted anywhere and updated in the ${project-folder}/artefacts/versions.json
+
+```json
+{
+  "versions": [
+    {
+      "version": "8.1.1",
+      "modelerUrl": "http://files.herman.codes/mx-files/mendix-8.1.1.58432.zip"
+    }
+  ]
+}
+```
+
+Once a version is downloaded, it unzips the content in ${project-folder}/tmp which is the same folder 
+containing the zipfile.
--- src/commands/download-version.command.ts
+++ src/commands/download-version.command.ts
@@ -4,6 +4,8 @@ import {Argv} from "yargs";
 import * as http from "http";
 const unzip = require("unzip");
 
+import shell from "shelljs";
+
 const versions = require("../artefacts/versions.json");
 
 export default async (options: OptionsInterface, yargs: Argv) => {
@@ -12,6 +14,22 @@ export default async (options: OptionsInterface, yargs: Argv) => {
     );
     if (version === undefined) throw new Error(`Version ${yargs.argv.mendixVersion} is invalid`);
     const cachePath = `${options.tmp}/${version.modelerUrl.replace("http://files.herman.codes/mx-files/", "")}`;
+    const build = () => {
+        try {
+            let source = fs.readFileSync(`${__dirname}/../templates/Dockerfile`, 'utf8').toString();
+            source = source
+                .replace(/dockerImage/g, version.dockerImage)
+                .replace(/#RUN apt-get -y install openjdk-11-jre-headless/g, version.javaVersion === 11 ? "RUN apt-get -y install openjdk-11-jre-headless" : "");
+            if (fs.existsSync(`${__dirname}/../../tmp/${version.version}/Dockerfile`)) {
+                fs.unlinkSync(`${__dirname}/../../tmp/${version.version}/Dockerfile`);
+            }
+            console.log(`Creating ${`${__dirname}/../../tmp/${version.version}/Dockerfile`}`);
+            fs.writeFileSync(`${__dirname}/../../tmp/${version.version}/Dockerfile`, source);
+            shell.exec(`cd tmp/${version.version} && docker build -t mxbuild:${version.version} .`);
+        } catch (e) {
+            console.error(e);
+        }
+    };
     return new Promise<any>((fulfill, reject) => {
         if (!fs.existsSync(options.tmp)) {
             console.log(`Creating temp folder...`);
@@ -30,11 +48,13 @@ export default async (options: OptionsInterface, yargs: Argv) => {
                             .pipe(unzip.Extract({ path: options.tmp }))
                             .on('finish', function() {
                                 fulfill("Done");
+                                build();
                             });
                 });
             })
         } else {
             fulfill("Already cached");
+            build();
         }
     });
 }
